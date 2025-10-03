c3a2f1d4c57f9948bc549c23e71bf7cfb02fff73
David Yang <testzzo.no1@gmail.com>
2025-10-03 23:40:09 +0800

    feat: add playwright for root_agent

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -1,5 +1,8 @@
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
+from mcp import StdioServerParameters
 
 
 root_agent = Agent(
@@ -8,10 +11,26 @@ root_agent = Agent(
         model="ollama/qwen2.5vl:7b",
         api_base="http://140.116.8.202:30192", # 如果要用本機的話不用加這個
     ),
-    description=(
-        "Agent to answer questions about the anything."
-    ),
     instruction=(
-        "You are a helpful agent who can answer user questions about anything."
+        "You are an expert web automation agent. "
+        "You can navigate to web pages, take screenshots, fill forms, and click elements. "
+        "When asked to interact with a web page, use the appropriate browser tools. "
+        "If you don't know something, you can always google it using the browser tools"
     ),
+    tools=[
+        MCPToolset(
+            connection_params=StdioConnectionParams(
+                server_params=StdioServerParameters(
+                    command="npx",
+                    args=[
+                        "-y",
+                        "@playwright/mcp@latest",
+                        "--vision",
+                    ],
+                ),
+                timeout=60, # mcp，有需要可以在加長
+            )
+        )
+    ]
 )
+
